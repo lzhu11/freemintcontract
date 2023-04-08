727fa62f2b259d43dd88bc5b700bdc086f633f10
lzhu11 <a9604420@gmail.com>
2023-04-09 00:24:11 +0800

    0429
    record

---
+++ nft-collectible-frontend/build/precache-manifest.936522f7854dfc204f114d843086eb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aae861a468b54adca6ba",
+    "url": "/static/css/main.dbeaea45.chunk.css"
+  },
+  {
+    "revision": "aae861a468b54adca6ba",
+    "url": "/static/js/main.aae861a4.chunk.js"
+  },
+  {
+    "revision": "e2723de9d6e9385cbe1e",
+    "url": "/static/js/1.e2723de9.chunk.js"
+  },
+  {
+    "revision": "12a11693010a6a9e762a",
+    "url": "/static/js/2.12a11693.chunk.js"
+  },
+  {
+    "revision": "56b2c7d1e2d5f244900f",
+    "url": "/static/js/runtime~main.56b2c7d1.js"
+  },
+  {
+    "revision": "5c47ea7f7491a734605578169819d458",
+    "url": "/index.html"
+  }
+];
